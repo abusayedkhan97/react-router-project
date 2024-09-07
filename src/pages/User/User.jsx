ca9130dfccf3ee69644deec154d997286d8800ad
Abu sayed khan <sdbiplop@gmail.com>
2024-09-07 21:25:42 +0600

    abc

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -3,8 +3,8 @@ import { user } from "../../fake/user";
 const User = () => {
   return (
     <>
-        <div className="container flex flex-wrap gap-3 p-10 mx-auto">
         <h1 className="text-center font-bold text-4xl py-10">Our Team Membar</h1>
+        <div className="container flex flex-wrap gap-3 p-10 mx-auto">
             {user.map((item)=>{
                 return (
                     <div className="user w-[19%] border p-5" key={item}>
