40a1cb4b3a38793e509f280d1d2b3addee29c7f4
Abu sayed khan <sdbiplop@gmail.com>
2024-09-07 21:04:25 +0600

    first commit

---
+++ src/fake/user.js
@@ -0,0 +1,47 @@
+export const user = [
+    {
+        id: 1,
+        name: "Abu Sayed khan",
+        photo: "https://scontent.fdac96-1.fna.fbcdn.net/v/t39.30808-1/454669711_3793526384306943_1574654444811937603_n.jpg?stp=dst-jpg_s200x200&_nc_cat=110&ccb=1-7&_nc_sid=50d2ac&_nc_eui2=AeH8X3VOLWWY7g2005CsU0-Dbw3hlV1kPYJvDeGVXWQ9gso8vWyc9x2j71Y6iMDKNXYAT-ctgzHEWkqCOInvblAM&_nc_ohc=wFdAuxo0Pc8Q7kNvgFxCkxi&_nc_ht=scontent.fdac96-1.fna&_nc_gid=AU-AT8zd38GR9BTSpPEZ_Vm&oh=00_AYAi2kAsqcU9pTbiv9iDHdJAR54XnYU0hkIKuKv2WC1kKA&oe=66E24376",
+    },
+    {
+        id: 2,
+        name: "John Advin",
+        photo: "https://scontent.fdac96-2.fna.fbcdn.net/v/t39.30808-1/340934430_616215933716507_8547407267766860164_n.jpg?stp=dst-jpg_s200x200&_nc_cat=105&ccb=1-7&_nc_sid=50d2ac&_nc_eui2=AeEbxhH3FPCX2r2L2vQfBEDmfZ9dyKuonzt9n13Iq6ifO8Sbia7hYrZpEHaXFKT5liU-EgvGCcCfY2tu_Pl3Nbj9&_nc_ohc=iOn2OFEFM3IQ7kNvgFQ-vCL&_nc_ht=scontent.fdac96-2.fna&oh=00_AYDZdNicGpbq3FelkVVSgCHeq2Fou4he8q06AWVhRvopHw&oe=66E22F97",
+    },
+    {
+        id: 3,
+        name: "Md. Helal Uddin",
+        photo: "https://scontent.fdac96-2.fna.fbcdn.net/v/t39.30808-1/402131052_6791157917620660_2962113155705074374_n.jpg?stp=dst-jpg_s200x200&_nc_cat=108&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeE_K1Dw55ntgSR_tmRanONBclNH5TQaYi9yU0flNBpiL_KJEDgK2w6_moVmF4rtIldRI3yrlXxeJqYZHlIXTg8c&_nc_ohc=i27qg9mZL1EQ7kNvgHMTjSq&_nc_ht=scontent.fdac96-2.fna&oh=00_AYCt1WjEewKBv7rgOmOAQRJTPCo7L3hnSxsAqRBw0TTu3w&oe=66E22585",
+    },
+    {
+        id: 4,
+        name: "Wing Shots",
+        photo: "https://scontent.fdac96-1.fna.fbcdn.net/v/t39.30808-1/277229869_107063388621730_3779644358424518121_n.jpg?stp=dst-jpg_s200x200&_nc_cat=102&ccb=1-7&_nc_sid=f4b9fd&_nc_eui2=AeGBx0p5oBNwEr-3a99Gd0BMMS5pUW2lnzkxLmlRbaWfOWKBe4v5MX7iNVvKvaiPho6z9gQgrwW9zuJz6PQa1lua&_nc_ohc=rBEezB2rjwYQ7kNvgEA_RzT&_nc_ht=scontent.fdac96-1.fna&oh=00_AYCkw4Dhu2sLstsWim_R4jflZe4d75kIFvK_HKrjdn1CHQ&oe=66E23D69",
+    },
+    {
+        id: 5,
+        name: "Fly By Fuad",
+        photo: "https://scontent.fdac96-1.fna.fbcdn.net/v/t39.30808-1/450854767_891166732829639_2936787550576070717_n.jpg?stp=dst-jpg_s200x200&_nc_cat=102&ccb=1-7&_nc_sid=f4b9fd&_nc_eui2=AeHPFz31zPsc29Av1oBqH_k8Pb5b0Bldu3A9vlvQGV27cGhzhYhTHpnwDm0kjFSnN3ea7cy37I3Zc_yPDaz7UZOh&_nc_ohc=edId_wb1CtkQ7kNvgFH99SL&_nc_ht=scontent.fdac96-1.fna&oh=00_AYDURne_ZMTqaEskT8Z1mrjwUwW7gD2Kbj4ajHWZlSQGBQ&oe=66E22E92",
+    },
+    {
+        id: 6,
+        name: "Riajul Hasan",
+        photo: "https://scontent.fdac96-1.fna.fbcdn.net/v/t1.6435-1/80061658_1167727613426218_4647948569041436672_n.jpg?stp=dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=db5862&_nc_eui2=AeGxwxp6XzcWYNiTfrasgkoPJ1C4VMVZEFknULhUxVkQWdX8-Efi_3uy0ab7THT_J-7OneQKbuq6nYiOb5O70PkQ&_nc_ohc=ZHPl1GZbNyYQ7kNvgHlIzMf&_nc_ht=scontent.fdac96-1.fna&oh=00_AYBLvadIcZDmgiEqV_YZovvoIArPMWasV24Oxc1Q4mxeQw&oe=6703C65F",
+    },
+    {
+        id: 7,
+        name: "Flying Eye",
+        photo: "https://scontent.fdac96-2.fna.fbcdn.net/v/t39.30808-6/454739303_122159915270140513_6577236042458271785_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFqU04-3jE2_kzAhHsyO4czinp4rugTZw2Keniu6BNnDUWck9gG7Hd8e-PN7sfUSLlXbdtMFJWHE-RdI1bsT3mA&_nc_ohc=kbp57SJAYHgQ7kNvgFa_HYA&_nc_zt=23&_nc_ht=scontent.fdac96-2.fna&oh=00_AYBnWnmjpeYrr9U-2Rgjke426m2KMJVsqIf_xNIE2QrycA&oe=66E235BE",
+    },
+    {
+        id: 8,
+        name: "Md Hridoy",
+        photo: "https://scontent.fdac96-2.fna.fbcdn.net/v/t39.30808-1/453808064_1641261196663994_2913143968169738146_n.jpg?stp=dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeE6uDxuyENJBYFA1eRBiofJFzwra3IeHLsXPCtrch4cuxvYA8izrldcyRppnSRRnEdhtM4_HmGawfrZ8aVKN-FX&_nc_ohc=i6XAyn7k9dgQ7kNvgEpmvoX&_nc_ht=scontent.fdac96-2.fna&_nc_gid=A-7iwfUEOjqzdsx4LLLRO9N&oh=00_AYCeH2EP_e5kwtMTV4JLK27WkoKv-KkL1YNYqK65pGC67Q&oe=66E23109",
+    },
+    {
+        id: 9,
+        name: "Rafi Sabbir",
+        photo: "https://scontent.fdac96-1.fna.fbcdn.net/v/t39.30808-1/435559482_925992875937548_7883287887864856707_n.jpg?stp=dst-jpg_s200x200&_nc_cat=107&ccb=1-7&_nc_sid=50d2ac&_nc_eui2=AeFVXobBa5fkmjo-YDnpMQIUJG2mkPB54lkkbaaQ8HniWXDUQnmunvU_dj498BuaDvaQQZb4pE9T41eKe1nU-94x&_nc_ohc=tVyIt37E61YQ7kNvgEWaDvg&_nc_ht=scontent.fdac96-1.fna&_nc_gid=APcbl41g6zO3cb__bZYyBXU&oh=00_AYCKOhj1D0HERGFWcFU8Ad_jIwgbeldKhorxgKNl8-v0XQ&oe=66E240D2",
+    },
+];
---
+++ src/pages/User/User.jsx
@@ -0,0 +1,20 @@
+import { user } from "../../fake/user";
+
+const User = () => {
+  return (
+    <>
+        <div className="container flex flex-wrap gap-3 p-10 mx-auto">
+            {user.map((item)=>{
+                return (
+                    <div className="user w-[19%] border p-5" key={item}>
+                        <img className="w-[200px] h-[200px] object-cover rounded-2xl mx-auto" src={item.photo} alt="" />
+                        <h1 className="text-2xl mt-3 text-center font-bold text-emerald-600">{item.name}</h1>
+                    </div>
+                );
+            })}
+        </div>
+    </>
+  )
+}
+
+export default User
